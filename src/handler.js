22464036856de2bda56650856f72b37fa1f3e635
mazufik <taufikprincecode10@gmail.com>
2023-06-25 13:51:56 +0900

    refactoring fitur add note

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,7 @@
 import { nanoid } from "nanoid";
 import notes from "./notes.js";
 
-const addNoteHandler = (request, h) => {
+const addNoteHandler = (request, response) => {
   const { title, tags, body } = request.payload;
 
   const id = nanoid(16);
@@ -22,25 +22,21 @@ const addNoteHandler = (request, h) => {
   const isSuccess = notes.filter((note) => note.id === id).length > 0;
 
   if (isSuccess) {
-    const response = h.response({
+    return response({
       code: 201,
       status: "success",
       message: "Note success to add",
       data: {
         notes,
       },
-    });
-    response.code(201);
-    return response;
+    }).code(201);
   }
 
-  const response = h.response({
+  return response({
     code: 500,
     status: "fail",
     message: "Note failed to add",
-  });
-  response.code(500);
-  return response;
+  }).code(500);
 };
 
 export default addNoteHandler;
